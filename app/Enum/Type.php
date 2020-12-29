0e6e487f90897db1ef1b53fe4e1b6f80db54b44f
H37kouya <h37kouya@gmail.com>
2020-12-29 23:00:13 +0900

    init

---
+++ app/Enum/Type.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enum;
+
+/**
+ * [Enum] Type
+ */
+class Type
+{
+    /**
+     * ユーザー
+     */
+    const USER = 'USER';
+
+    /**
+     * 組織
+     */
+    const ORGANIZATION  = 'ORGANIZATION';
+}
